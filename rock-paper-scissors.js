af6666f1589293326d62c73c8bca631dad507620
akaAndrewS <132374843+akaAndrewS@users.noreply.github.com>
2023-05-22 12:22:50 -0400

    Add pseudocode for getComputerChoice function to rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,6 @@
+// function getComputerChoice() that represents computer's choice and returns either rock, paper or scissors
+// variable 'computerChoice' to be used to store the computer's choice
+// variable 'result' to be used to store result, default is 0
+// sets 'result' to a random number between 0-2
+// sets 'computerChoice' to correct option based on 'result'
+// returns 'computerChoice'
